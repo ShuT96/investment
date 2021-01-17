f88ec517712beaa0ac17437f8e322497b5a91fa6
ShuT96 <shusuke.takamizawa@gmail.com>
2021-01-17 10:26:26 +0900

    adding minimum return scenario for ETFs

--- mutual_fund/etf_efficient_frontier.py
+++ mutual_fund/etf_efficient_frontier.py
@@ -7,16 +7,17 @@ from pandas_datareader import data
 
 num_port = 20000
 rf = 0.005
-days = 252
+days = 260
 alpha = 0.05
-tickers = ['1321', '1546', '1547', '1566', '2511', '2515']
+tickers = ['1321.T', '1546.T', '1547.T', '1566.T', '2511.T', '2515.T']
 
 
-def adj_close_df():
-    start = datetime(2015, 1, 1)
-    end = datetime(2018, 1, 1)
+
+def adj_close_df(tickers=None):
+    start = datetime(2019, 1, 1)
+    end = datetime(2020, 12, 31)
     df = pd.DataFrame([data.DataReader(ticker, 'yahoo', start, end)['Close'] for ticker in tickers]).T
-    print(df)
+    df = df.sort_index()
     df.columns = tickers
     print(df.head())
     mean_returns = df.pct_change().mean()
@@ -55,6 +56,7 @@ def show_allocation_result_portfolios(results_df):
     min_var_port = results_df.iloc[results_df['VaR'].idxmin()]
     max_sharpe_port = results_df.iloc[results_df['Sharpe'].idxmax()]
     min_vol_port = results_df.iloc[results_df['StDev'].idxmin()]
+    min_ret_port = results_df.iloc[results_df['Return'].idxmin()]
 
     print('-' * 80)
     print('Max Return allocation is:\n')
@@ -69,11 +71,13 @@ def show_allocation_result_portfolios(results_df):
     print('Max Sharpe Ratio allocation is:\n')
     print(max_sharpe_port.to_frame().T)
     print('-' * 80)
+    print('Minimum Return allocation is:\n')
+    print(min_ret_port.to_frame().T)
 
-    return min_var_port, max_sharpe_port, min_vol_port, max_return_port
+    return min_var_port, max_sharpe_port, min_vol_port, max_return_port, min_ret_port
 
 
-def plot_efficient_frontier(results_df, min_VaR_port, max_sharpe_port, min_vol_port, max_return_port):
+def plot_efficient_frontier(results_df, min_VaR_port, max_sharpe_port, min_vol_port, max_return_port, min_ret_port):
     plt.subplots(figsize=(15, 10))
     plt.scatter(results_df.StDev, results_df.Return, c=results_df.Sharpe, cmap='RdYlBu')
     plt.xlabel('Standard Deviation')
@@ -84,16 +88,16 @@ def plot_efficient_frontier(results_df, min_VaR_port, max_sharpe_port, min_vol_p
     plt.scatter(min_VaR_port[1], min_VaR_port[0], marker=(5, 1, 0), color='b', s=200)
     plt.scatter(max_sharpe_port[1], max_sharpe_port[0], marker=(5, 1, 0), color='r', s=200)
     plt.scatter(min_vol_port[1], min_vol_port[0], marker=(5, 1, 0), color='g', s=200)
+    plt.scatter(min_ret_port[1], min_ret_port[0], marker=(5, 1, 0), color='pink', s=200)
 
     plt.show()
 
 
 def main():
-
-    df = adj_close_df()
+    df = adj_close_df(tickers=tickers)
     result_pf_df = simulate_random_portfolios(num_port, df[1], df[2], alpha, rf, days)
     opt_allo_df = show_allocation_result_portfolios(result_pf_df)
-    plot_efficient_frontier(result_pf_df, opt_allo_df[0], opt_allo_df[1], opt_allo_df[2], opt_allo_df[3])
+    plot_efficient_frontier(result_pf_df, opt_allo_df[0], opt_allo_df[1], opt_allo_df[2], opt_allo_df[3], opt_allo_df[4])
 
 
 if __name__ == '__main__':
